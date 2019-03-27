7ccb213a213174d324b89adee8ab1fe4e61ed8d2
gcr <gcr1208@163.com>
2019-03-27 15:30:43 +0800

    解决install编译名字总是microservice问题，修改编译jar包为war包，优化部署tomcat打包为war包后依然报错问题 （主要修改启动类要去除springboot自身的tomcat）

--- microservicecloud-eureka-7001/src/main/java/com/atguigu/springcloud/EurekaServer7001_App.java
+++ microservicecloud-eureka-7001/src/main/java/com/atguigu/springcloud/EurekaServer7001_App.java
@@ -2,12 +2,24 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer//EurekaServer服务器端启动类,接受其它微服务注册进来
-public class EurekaServer7001_App
+public class EurekaServer7001_App extends SpringBootServletInitializer
+//public class EurekaServer7001_App
 {
+
+    /**
+     * 项目更改为tomcat启动时，需重写configure方法
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EurekaServer7001_App.class);
+    }
+
     public static void main(String[] args)
     {
         SpringApplication.run(EurekaServer7001_App.class, args);
--- microservicecloud-eureka-7002/src/main/java/com/atguigu/springcloud/EurekaServer7002_App.java
+++ microservicecloud-eureka-7002/src/main/java/com/atguigu/springcloud/EurekaServer7002_App.java
@@ -2,14 +2,26 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer//EurekaServer服务器端启动类,接受其它微服务注册进来
-public class EurekaServer7002_App
+public class EurekaServer7002_App extends SpringBootServletInitializer
+//public class EurekaServer7002_App
 {
+
+    /**
+     * 项目更改为tomcat启动时，需重写configure方法
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EurekaServer7002_App.class);
+    }
+
     public static void main(String[] args)
     {
-        SpringApplication.run(EurekaServer7002_App.class, args);
+       SpringApplication.run(EurekaServer7002_App.class, args);
     }
 }
